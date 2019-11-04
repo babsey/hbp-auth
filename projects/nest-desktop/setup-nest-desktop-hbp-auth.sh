5e994e31781f458d3fd0ae361e2d05813dee93ae
Sebastian Spreizer <spreizer@web.de>
2019-11-04 12:58:10 +0100

    Update setup

--- projects/nest-desktop/setup-nest-desktop-hbp-auth.sh
+++ projects/nest-desktop/setup-nest-desktop-hbp-auth.sh
@@ -8,4 +8,4 @@ oc delete is,dc,svc,route,configMap nest-desktop-hbp-auth
 oc new-app babsey/nest-desktop-hbp-auth
 oc new-app ./openshift/nest-desktop-hbp-auth.yaml
 oc set env --from='configmap/nest-desktop-hbp-auth' dc/nest-desktop-hbp-auth
-oc expose svc/nest-desktop-hbp-auth --hostname='nest-desktop.apps.hbp.eu' --port='8080'
+oc create route edge --hostname='nest-desktop.apps.hbp.eu' --port='8080' --insecure-policy='Redirect' --service='nest-desktop-hbp-auth'
