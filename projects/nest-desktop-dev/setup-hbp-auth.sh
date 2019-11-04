2b0f5afaf2de017c22ed4b2c538a56d2956e7a1d
Sebastian Spreizer <spreizer@web.de>
2019-11-04 11:47:23 +0100

    Rename image of docker hub

--- projects/nest-desktop-dev/setup-hbp-auth.sh
+++ projects/nest-desktop-dev/setup-hbp-auth.sh
@@ -4,7 +4,7 @@
 
 oc project nest-desktop-dev
 oc delete is,dc,svc,route,configMap hbp-auth
-oc new-app babsey/hbp-auth
+oc new-app babsey/nest-desktop-hbp-auth
 oc new-app ./openshift-dev/hbp-auth-dev.yaml
 oc set env --from="configmap/hbp-auth" dc/hbp-auth
 oc create route edge --hostname="nest-desktop.apps-dev.hbp.eu" --port=8080 --insecure-policy="Redirect" --service="hbp-auth"
